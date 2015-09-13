fbb0ce413a42d1e93bbc69e8f653db57f6d6e241
Joseph M. Lloyd <joe.lloyd.22.24@gmail.com>
2015-09-13 23:37:14 +0100

    added templates and states

--- app/app.module.js
+++ app/app.module.js
@@ -2,6 +2,7 @@ var app = angular.module("myApp", ['ui.bootstrap', 'ngAnimate', 'ui.router']);
 
 app.controller('navCtrl', ['$scope', function($scope){
 	$scope.menuItems = [{title: 'About'}, {title: 'Services'}, {title: 'Contact'}];
+	
 }]);
 
 app.config(function($stateProvider, $urlRouterProvider) {
@@ -13,12 +14,25 @@ app.config(function($stateProvider, $urlRouterProvider) {
         // HOME STATES AND NESTED VIEWS ========================================
         .state('home', {
             url: '/home',
-            //templateUrl: 'partial-home.html'
+            templateUrl: 'app/components/home/partial-home.html'
         })
         
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-        .state('about', {
-            // we'll get to this in a bit       
+        .state('About', {
+            url: '/About',
+            templateUrl: 'app/components/about/about.html'      
+        })
+
+        // SERVICES PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('Services', {
+            url: '/Services',
+            templateUrl: 'app/components/services/services.html'      
+        })
+
+        // CONTACT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('Contact', {
+            url: '/Contact',
+            templateUrl: 'app/components/contact/contact.html'      
         });
         
 });
---
+++ app/components/home/homeCtrl.js
@@ -0,0 +1,1 @@
+homeCtrl.js
