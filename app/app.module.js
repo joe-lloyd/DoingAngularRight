638a3a3ddc4ab732a0b5704124c5c6bdfa57b6b5
Joseph M. Lloyd <joe.lloyd.22.24@gmail.com>
2015-09-13 15:54:52 +0100

    started adding routing with state provider and ui-router

--- app/app.module.js
+++ app/app.module.js
@@ -1,6 +1,24 @@
-var app = angular.module("myApp", ['ui.bootstrap', 'ngAnimate']);
+var app = angular.module("myApp", ['ui.bootstrap', 'ngAnimate', 'ui.router']);
 
 app.controller('navCtrl', ['$scope', function($scope){
-	$scope.navbarCollapsed = true;
 	$scope.menuItems = [{title: 'About'}, {title: 'Services'}, {title: 'Contact'}];
 }]);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            //templateUrl: 'partial-home.html'
+        })
+        
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+            // we'll get to this in a bit       
+        });
+        
+});
